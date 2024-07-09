ef34e5c9e6b2779cf6611a77daaa16c2c044b0e3
Guilherme Candido <teoriaeletrica@gmail.com>
2024-07-09 13:40:52 +0100

    -

--- LabView Library/Example/cansart_db.h
+++ LabView Library/Example/cansart_db.h
@@ -35,32 +35,4 @@ struct frame10
     uint8_t LENGHT = 8;
 };
 
-struct frame23
-{
-    uint8_t ID = 23;
-    uint8_t DATA1 = 0;
-    uint8_t DATA2 = 0;
-    uint8_t DATA3 = 0;
-    uint8_t DATA4 = 0;
-    uint8_t DATA5 = 0;
-    uint8_t DATA6 = 0;
-    uint8_t DATA7 = 0;
-    uint8_t DATA8 = 0;
-    uint8_t LENGHT = 8;
-};
-
-struct frame121
-{
-    uint8_t ID = 121;
-    uint8_t DATA1 = 0;
-    uint8_t DATA2 = 0;
-    uint8_t DATA3 = 0;
-    uint8_t DATA4 = 0;
-    uint8_t DATA5 = 0;
-    uint8_t DATA6 = 0;
-    uint8_t DATA7 = 0;
-    uint8_t DATA8 = 0;
-    uint8_t LENGHT = 8;
-};
-
 #endif
